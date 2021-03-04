ce4703c243a4ccfb749df5463137f443be574f8d
pallavi.gupta <pallavi.gupta@kochartech.com>
2021-03-04 10:28:02 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+
+function hello() {
+  return (
+    <div className="hello-world">
+        <img src={"https://i.pinimg.com/originals/9e/a7/2e/9ea72ef078139ced289852e8a4ea0c5c.gif"} alt="logo" />
+        <h1>
+         Happy Hacking 
+        </h1>
+    </div>
+  );
+}
+
+export default hello;
---
+++ src/Tick.js
@@ -0,0 +1,33 @@
+import React, { Component } from "react";
+
+function Tick() {
+  return (
+    <div>
+      <h1>Hello, world!</h1>
+      <h2>It is {new Date().toLocaleTimeString()}.</h2>
+    </div>
+  );
+}
+
+// class Tick extends Component {
+//   constructor() {
+//     super();
+//     this.state = {
+//       name: "Jorav",
+//       age: 23,
+//     };
+//   }
+
+//   render() {
+//     return (
+//       <div>
+//         <h1>Hello, world!</h1>
+//         <h2>
+//           This is {this.state.name} and I am {this.state.age} years old.
+//         </h2>
+//       </div>
+//     );
+//   }
+// }
+
+export default Tick;
