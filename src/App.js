baf720bffae42e15fea3d9a9597f2d6ef0cf3470
pallavi.gupta <pallavi.gupta@kochartech.com>
2021-03-05 10:23:48 +0530

    counter

--- src/App.js
+++ src/App.js
@@ -1,14 +1,49 @@
 import './App.css';
+import React, { Component } from 'react';
 
-function hello() {
-  return (
-    <div className="hello-world">
-        <img src={"https://i.pinimg.com/originals/9e/a7/2e/9ea72ef078139ced289852e8a4ea0c5c.gif"} alt="logo" />
-        <h1>
-         Happy Hacking 
-        </h1>
-    </div>
-  );
+import Button from './Button';
+
+
+// function hello() {
+//   return (
+//     <div className="hello-world">
+//         <img src={"https://i.pinimg.com/originals/9e/a7/2e/9ea72ef078139ced289852e8a4ea0c5c.gif"} alt="logo" />
+//         <h1>
+//          Happy Hacking 
+//         </h1>
+//     </div>
+//   );
+// }
+
+// export default hello;
+
+
+class App extends Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      count: 1
+    }
+
+  }
+
+  handleCount(value) {
+    this.setState((prevState) => ({ count: prevState.count + value }));
+  }
+
+  render() {
+
+
+    return (
+      <div>
+        Current count: {this.state.count}
+        <hr />
+        <Button sign="+" count={this.state.count} updateCount={this.handleCount.bind(this)} />
+        <Button sign="-" count={this.state.count} updateCount={this.handleCount.bind(this)} />
+      </div>
+    );
+  }
 }
 
-export default hello;
+export default App;
